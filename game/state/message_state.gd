2675df75a667bfbdc6932d4720bc9a0a49396d3b
LegenDarius <darius@ddegel.ch>
2025-04-28 15:17:07 +0200

    only fetch messages sent after joining

--- game/state/message_state.gd
+++ game/state/message_state.gd
@@ -2,7 +2,8 @@ class_name MessageState
 
 extends State
 
+const x = "2025-04-28T11:55:22.656161+00:00"
 func _init() -> void:
 	table_name = "message"
-	query = "SELECT * FROM message"
+	query = "SELECT * FROM message WHERE Sent > '%s.000000+00:00'" % Time.get_datetime_string_from_system(true)
 	super._init()
